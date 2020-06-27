2e30f1b2ff41dc9712c6e949803e47d6d9bb4274
Felipe Miranda <felip.simi@gmail.com>
2020-06-27 13:49:53 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/trilhainicial/responsestatus/ResponseStatusApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.responsestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtrilha/trilhainicial/responsestatus/ResponseStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.responsestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
